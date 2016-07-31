15925fbfcaed4bb489f43831289fd20423f0e55a
pranavidandu <dandupranavi@gmail.com>
2016-07-31 12:04:48 +0530

    Update wsgi.py

--- scribble/wsgi.py
+++ scribble/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for wildblogger project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# assuming your django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/pranavidandu/scribble/scribble'
+if path not in sys.path:
+    sys.path.append(path)
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wildblogger.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for django >=1.5:
 from django.core.wsgi import get_wsgi_application
-
 application = get_wsgi_application()
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wildblogger.settings")
